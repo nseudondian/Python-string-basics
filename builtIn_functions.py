7b6838a47f783d9408497ed36ad5d53e70f7f5d9
Nseabasi Udondian <nseabasiudondian@gmail.com>
2023-06-15 23:57:11 +0100

    edited-main

---
+++ builtIn_functions.py
@@ -0,0 +1,66 @@
+#Built-in Functions 
+
+#len() -Returns the length of an object
+
+#absolute count - Count starting from 1, not 0
+
+# msg = "hello world!"
+
+# msg_length = len(msg)
+
+# msg_index = len(msg) - 1
+
+# print(msg_length)
+# print(msg_index)
+
+# print(msg[11])
+
+#input() -  It is used to prompt the user for input and returns the entered value as a string.
+
+# age = input("How old are you ?")
+
+# print("I am " + age + " years old")
+
+# age = 18
+
+# print(age)
+# print("age")
+#str() - converts a value to a string data type
+#int() - converts a value to an integer data type
+#float() - converts a value to a floating-point number
+
+#Type casting - Conversion from one data type to another
+
+# converting int to str
+# print(type(10))
+
+# val = str(10)
+
+# print(type(val))
+
+# converting flaot to str
+# print(type(10.00))
+
+# val = str(10.00)
+
+# print(type(val))
+
+#converting float to int using int()
+
+# print(type(10.00))
+
+# val = int(10.00)
+
+# print(type(val))
+
+#converting str to float using float()
+
+# print(type("10"))
+
+# val = float("10")
+
+# print(type(val))
+
+# help() - provides interactive help and documentation
+
+help(str)
